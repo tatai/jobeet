70f8a60bc431f30857f174be4d495c5e50e00761
tatai <fran.naranjo@gmail.com>
2010-02-05 04:00:58 -0800

    8. Testing slugify

--- test/unit/JobeetTest.php
+++ test/unit/JobeetTest.php
@@ -1,6 +1,13 @@
 <?php
 require_once(dirname(__FILE__) . '/../bootstrap/unit.php');
 
-$t = new lime_test(1);
-$t->pass('This test always passes');
+$t = new lime_test(6);
+ 
+$t->comment('::slugify()');
+$t->is(Jobeet::slugify('Sensio'), 'sensio', '::slugify() converts all characters to lower case');
+$t->is(Jobeet::slugify('sensio labs'), 'sensio-labs', '::slugify() replaces a white space by a -');
+$t->is(Jobeet::slugify('sensio   labs'), 'sensio-labs', '::slugify() replaces several white spaces by a single -');
+$t->is(Jobeet::slugify('  sensio'), 'sensio', '::slugify() removes - at the beginning of a string');
+$t->is(Jobeet::slugify('sensio  '), 'sensio', '::slugify() removes - at the end of a string');
+$t->is(Jobeet::slugify('paris,france'), 'paris-france', '::slugify() replaces non-ASCII characters by a -');
 ?>
